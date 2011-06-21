6acf226e1c7ab077292443227c29640251e957b7
franck cuny <franck@lumberjaph.net>
2011-06-21 14:08:01 +0200

    add more attributes
    Signed-off-by: franck cuny <franck@lumberjaph.net>

--- spyre/request.py
+++ spyre/request.py
@@ -1,6 +1,21 @@
 class Request:
-    def __init_(self, env):
+    def __init_(self, **env):
         self.env = env
-
+        self.path = self.env['PATH_INFO']
+        self.method = self.env['REQUEST_METHOD']
+        self.port = self.env['SERVER_PORT']
+        self.request_uri = self.env['REQUEST_URI']
+        self.scheme = self.env['spore.scheme']
+        self.script_name = self.env['SCRIPT_NAME']
+        self.body = self.env['spore.payload']
+        
     def finalize(self):
         # build request
+        1
+
+    def secure(self):
+        self.scheme == 'https'
+
+    def uri(self, path_info=None, query_string=None):
+        1
+
