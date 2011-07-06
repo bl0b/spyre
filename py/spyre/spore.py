e64602e4e6c4e38c6fbc8a3b3b1feb84d30d1676
franck cuny <franck@lumberjaph.net>
2011-07-06 23:22:50 +0200

    more code stolen from sandbox
    Signed-off-by: franck cuny <franck@lumberjaph.net>

--- py/spyre/spore.py
+++ py/spyre/spore.py
@@ -1,7 +1,48 @@
+import spore.request
+
+
 def spore_method(name, spec):
     class spore_method_handler(object):
         def __init__(self, name):
-            pass
+            self.im_func = self
+            self.im_self = None
+            self.im_class = spore_method_handler
+            self.name = name
+            self.req = spec['required_params']
+            self.opt = safe(spec, 'optional_params', [])
+            self.path = spec['path']
+            self.method = spec['method']
+
+        def __check_args(self, args, kwargs):
+            return validate_arguments(self, args, kwargs)
+
+        def expand(self, txt, dic):
+            for k, v in dic.iteritems():
+                txt = v.join(txt.split(':' + k))
+            return txt
+
+        def __call__(self, spo, *args, **kwargs):
+            try:
+                arg_dic = self.__check_args(args, kwargs)
+                env = {
+                    'REQUEST_METHOD' = self.method,
+                }
+                spo._exec_call(request(method=self.method, url=self.expand(self.path, arg_dic)))
+            except http_call_exception, hce:
+                print hce
+                if http_call_fail_silently:
+                    return None
+                else:
+                    raise hce
+
+        def __get__(self, i, c):
+            ret = lambda *a, **k: self(i, *a, **k)
+            ret.__doc__ = self.__doc__
+            ret.__name__ = str(self.name)
+            return ret
+
+    return spore_method_handler
+
 
 def new_from_spec(name, spec=None, **named):
     if not spec:
@@ -14,6 +55,7 @@ def new_from_spec(name, spec=None, **named):
             spore_method = spore_method(mname, mspec)
             dic[mname] = spore_method
     return type(name, (spore_base,), dic)
+
 
 class spore_meta(object):
     def __init__(class, name, bases, dic):
