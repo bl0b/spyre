378bb14a4b6e71ab0f36da53dad4d0f0561c5bc8
Damien Leroux <damien.leroux@gmail.com>
2011-07-06 21:45:52 +0200

    some editing

--- py/sandbox.py
+++ py/sandbox.py
@@ -55,6 +55,39 @@ spec2 = """
 }
 """
 
+
+#        my $env = {
+#            REQUEST_METHOD => $method->method,
+#            SERVER_NAME    => $base_url->host,
+#            SERVER_PORT    => $base_url->port,
+#            SCRIPT_NAME    => (
+#                $base_url->path eq '/'
+#                ? ''
+#                : $base_url->path
+#            ),
+#            PATH_INFO               => $method->path,
+#            REQUEST_URI             => '',
+#            QUERY_STRING            => '',
+#            HTTP_USER_AGENT         => $self->api_useragent->agent,
+#            'spore.expected_status' => [ $method->expected_status ],
+#            'spore.authentication'  => $authentication,
+#            'spore.params'          => $params,
+#            'spore.payload'         => $payload,
+#            'spore.errors'          => *STDERR,
+#            'spore.url_scheme'      => $base_url->scheme,
+#            'spore.formats'         => $formats,
+#        };
+#
+#        $env->{'spore.form_data'} = $method->form_data
+#          if $method->has_form_data;
+#
+#        $env->{'spore.headers'} = $method->headers if $method->has_headers;
+#
+#
+
+
+
+
 class http_call_exception(Exception):
     def __init__(self, *a, **k):
         Exception.__init__(self, *a, **k)
@@ -64,9 +97,40 @@ http_call_fail_silently = True
 
 safe = lambda d, k,t=None: k in d and d[k] or t
 
+# external definition
+def validate_arguments(self, args, kwargs):
+    arg_dic = None
+    # if using unnamed arguments (assumedly ONLY unnamed arguments)
+    if args:
+        if len(args)<len(self.req) or len(args) > (len(self.req)+len(self.opt)):
+            # we bad
+            if self.opt:
+                raise http_call_exception("Invalud argument count : expected between %i and %i arguments, got %i instead"%(len(self.req), len(self.opts), len(args)))
+            else:
+                raise http_call_exception("Invalud argument count : expected %i arguments, got %i instead"%(len(self.req), len(args)))
+        else:
+            # we good
+            arg_dic = dict(zip(self.req+self.opt, args))
+    else:
+        # otherwise we're using named arguments, just perform checks
+        kset = set(kwargs.iterkeys())   # all provided named arguments
+        req = set(self.req)             # all required arguments
+        opt = set(self.opt)             # all optional arguments
+        params = req.union(opt)         # all possible arguments
+        stuff_remains = kset.difference(req+opt)
+        if stuff_remains:
+            raise http_call_exception("Unknown argument names : "+', '.join(stuff_remains)+' (only the following names are defined :'+', '.join(params)+')')
+        missing_req = req.difference(kset)
+        if missing_req:
+            raise http_call_exception("Missing arguments : "+', '.join(missing_req))
+        arg_dic = kwargs
+    return arg_dic
+
+
 def http_call(name, spec):
     class http_call_handler(object):
         __doc__ = (""+safe(spec, 'documentation', '')+'\n'+spec['method']+' '+spec['path']).strip()
+
         def __init__(self, name):
             self.im_func = self
             self.im_self = None
@@ -75,34 +139,9 @@ def http_call(name, spec):
             self.req = spec['required_params'] # this one MUST NOT fail, so let the exception go
             self.opt = safe(spec, 'optional_params', [])
             self.authentication = safe(spec, 'authentication', False)=='true'
-        def __check_args(self, args, kwargs):
-            arg_dic = None
-            # if using unnamed arguments (assumedly ONLY unnamed arguments)
-            if args:
-                if len(args)<len(self.req) or len(args) > (len(self.req)+len(self.opt)):
-                    # we bad
-                    if self.opt:
-                        raise http_call_exception("Invalud argument count : expected between %i and %i arguments, got %i instead"%(len(self.req), len(self.opts), len(args)))
-                    else:
-                        raise http_call_exception("Invalud argument count : expected %i arguments, got %i instead"%(len(self.req), len(args)))
-                else:
-                    # we good
-                    arg_dic = dict(zip(self.req+self.opt, args))
-            else:
-                # otherwise we're using named arguments, just perform checks
-                kset = set(kwargs.iterkeys())
-                req = set(self.req)
-                opt = set(self.opt)
-                params = req.union(opt)
 
-                stuff_remains = kset.difference(self.req+self.opt)
-                if stuff_remains:
-                    raise http_call_exception("Unknown argument names : "+', '.join(stuff_remains)+' (only the following names are defined :'+', '.join(params)+')')
-                missing_req = req.difference(kset)
-                if missing_req:
-                    raise http_call_exception("Missing arguments : "+', '.join(missing_req))
-                arg_dic = kwargs
-            return arg_dic
+        def __check_args(self, args, kwargs):
+            return validate_arguments(self, args, kwargs)
 
         def expand(self, txt, dic):
             for k, v in dic.iteritems():
@@ -120,6 +159,7 @@ def http_call(name, spec):
                     return None
                 else:
                     raise hce
+
         def __get__(self, i, c):
             ret = lambda *a, **k: self(i, *a, **k)
             ret.__doc__ = self.__doc__
@@ -134,16 +174,19 @@ def http_call(name, spec):
 
 class spore_base(object):
     def __init__(self):
+        self.middlewares = []
         for mname in self.spore_spec:
             self.__getattr__(mname).__get__(self)
+    def reg_middleware(self, x):
+        self.middlewares.append(x)
 
-def spore(name, spec=None, **named):
+def new_from_spec(name, spec=None, **named):
     if not spec:
         spec = named
     # uncomment to override spec with given named args
     #spec.update(named)
     dic = {
-        'SPoREwashere': 42,
+        #'SPoREwashere': 42,
         '__doc__': safe(spec, 'documentation', '')+'\n'+str(spec),
         'version': safe(spec, 'version', 'please specify me'),
         'base_url': safe(spec, 'base_url', 'perdu.com'),
@@ -153,7 +196,6 @@ def spore(name, spec=None, **named):
         for mname, mspec in spec['methods'].iteritems():
             hc = http_call(mname, mspec)
             dic[mname] = hc
-            #dic[mname] = lambda self, *a, **k: hc(self, *a, **k)
     return type(name, (spore_base,), dic)
 
 
