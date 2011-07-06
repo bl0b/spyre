eab739f35156f029335692bd2e6f7579904113da
Damien Leroux <damien.leroux@gmail.com>
2011-07-06 23:39:47 +0200

    exploded sandbox from master branch into spore and http_call.

--- py/spyre/spore.py
+++ py/spyre/spore.py
@@ -1,24 +1,51 @@
-class spore(object):
+__all__ = ['new_from_spec']
+
+safe = lambda d, k,t=None: k in d and d[k] or t
+
+class spore_base(object):
     def __init__(self):
-        pass
+        self.middlewares = []
+        for mname in self.spore_spec:
+            self.__getattr__(mname).__get__(self)
+    def _iter_middlewares(self, req):
+        return (mw_func(req) for mw_pred, mw_func in self.middlewares if mw_pred(req))
+    def _run_middlewares(self, req, mwlist):
+        return map(lambda mw: mw(req), mwlist)
+    def enable(self, middleware, **kwargs):
+        self.enable_if(lambda req: True, middleware, **kwargs)
+    def enable_if(self, predicate, middleware, **kwargs):
+        if type(middleware) is str:
+            midmod = __import__(middleware, fromlist=['new']) # we actually get the whole module, the whole namespace
+            self.middlewares.append( (predicate, midmod.new(**kwargs)) )
+        elif callable(middleware):
+            self.middlewares.append( (predicate, middleware(**kwargs)) )
+        else:
+            raise ValueError(middleware)
+    def _exec_call(self, req):
+        resp_callbacks = self._run_middlewares(req, self._iter_middlewares(req))
+        # perform actual call with req
+        resp = None#FIXME
+        self._run_middlewares(resp, reversed(resp_callbacks))
+        return resp
 
-    def http_call():
-        class http_call_handler(object):
-            def __init__(self,spo):
-                pass
 
-            def __call__(self, *args,**kwargs):
-                print "pouet"
 
-class spore_meta(type):
-    def __init__(class, name, bases, dic):
-        spec = dic['spore_desc']
-        if 'documentation' in spec:
-            dic['__doc__'] = spec['documentation']
-            dic['base_url'] = spec['base_url']
-            dic['version'] = spec['version']
-        for mspec in spec['methods']:
-            pass
-        
-        
-        
+
+def new_from_spec(name, spec=None, **named):
+    if not spec:
+        spec = named
+    # uncomment to override spec with given named args
+    #spec.update(named)
+    dic = {
+        #'SPoREwashere': 42,
+        '__doc__': safe(spec, 'documentation', '')+'\n'+str(spec),
+        'version': safe(spec, 'version', 'please specify me'),
+        'base_url': safe(spec, 'base_url', 'perdu.com'),
+        'spore_spec': spec
+    }
+    if 'methods' in spec:
+        for mname, mspec in spec['methods'].iteritems():
+            hc = http_call(mname, mspec)
+            dic[mname] = hc
+    return type(name, (spore_base,), dic)
+
