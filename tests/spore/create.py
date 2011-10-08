6396f9ae86941e6e8db067a910c4469db03fd8a1
franck cuny <franck.cuny@gmail.com>
2011-10-08 15:51:53 -0700

    add a simple spec and a new test

--- tests/spore/create.py
+++
@@ -1,8 +0,0 @@
-from unittest2 import TestCase
-
-import spyre
-
-class TestSpyre(TestCase):
-    def test_create(self):
-        self.assertTrue(True)
-        pass
---
+++ tests/test_create.py
@@ -0,0 +1,19 @@
+from unittest2 import TestCase
+from spyre import Spyre
+import os.path
+
+
+MY_DIR = os.path.dirname(__file__)
+spec = MY_DIR + "/specs/api.json"
+
+
+class TestSpyre(TestCase):
+
+    def test_without_spec(self):
+        self.assertRaises(RuntimeError, Spyre.new_from_spec, ('spec'))
+
+    def test_create_from_spec(self):
+        spore = Spyre().new_from_spec(spec) 
+        self.assertTrue(spore)
+        self.assertEqual(spore.name, "Test API")
+        self.assertEqual(spore.get_info(), 5)
