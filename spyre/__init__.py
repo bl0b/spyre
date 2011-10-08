090f80b46aad2810a3f737d548fba29f5216b1e7
franck cuny <franck.cuny@gmail.com>
2011-10-08 15:52:13 -0700

    load a spec and attach methods

--- spyre/__init__.py
+++ spyre/__init__.py
@@ -3,10 +3,13 @@ import json
 from spyre import spyrecore
 
 
-class spyre(object):
+class Spyre(object):
+
     @classmethod
-    def new_from_spec(cls, spec):
-        pass
+    def new_from_spec(cls, spec_file, **args):
+        json = cls._read_from_file(spec_file)
+        spore = spyrecore.spyrecore(json)
+        return spore
 
     @classmethod
     def new_from_string(cls, spec):
@@ -15,3 +18,16 @@ class spyre(object):
     @classmethod
     def new_from_dict(cls, dict):
         pass
+
+    @classmethod
+    def _read_from_file(cls, spec_file):
+        spec = None
+        try:
+            f = open(spec_file)
+            spec = json.load(f)
+            f.close()
+        except Exception, e:
+            raise RuntimeError("Error while loading JSON spec (%s): %s"
+                    % (spec_file, e))
+
+        return spec
--- spyre/spyrecore.py
+++ spyre/spyrecore.py
@@ -1,3 +1,26 @@
-class spyrecore:
-    def __init__(self):
-        pass
+import __future__
+
+
+class spyrecore(object):
+    def __init__(self, json):
+        self.json = json
+        self._init_meta(json)
+        self._init_methods(json)
+
+    def _init_meta(self, json):
+        self.name = json['name']
+
+    def _init_methods(self, json):
+        for method_name, method_desc in json['methods'].iteritems():
+            self._attach_method(method_name, method_desc)
+
+    def _attach_method(self, method_name, method_desc):
+        setattr(self, method_name, httpcall(method_name, method_desc))
+
+
+class httpcall(object):
+    def __init__(self, name, desc):
+        self.name = name
+
+    def __call__(self):
+        return 5
