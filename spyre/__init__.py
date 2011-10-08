67d36d47cb1b58da66dff4e0db3f1ad2c24988e4
franck cuny <franck.cuny@gmail.com>
2011-10-08 14:20:02 -0700

    start final refactoring

---
+++ spyre/__init__.py
@@ -0,0 +1,17 @@
+import __future__
+
+class spyre(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def new_from_spec(cls, spec):
+        pass
+
+    @classmethod
+    def new_from_string(cls, spec):
+        pass
+
+    @classmethod
+    def new_from_dict(cls, dict):
+        pass
