e3583c8bb97748e928e0d7ad68ceb1d278f1a6e1
franck cuny <franck.cuny@gmail.com>
2011-10-09 15:03:11 -0700

    moar tests

--- tests/test_spyre_method.py
+++ tests/test_spyre_method.py
@@ -5,8 +5,32 @@ from spyre import errors
 
 class TestSpyreMethod(TestCase):
 
-    def test_errors(self):
+    def test_required_attr(self):
+        method_name = 'test'
         method_desc = {}
+
         self.failUnlessRaises(errors.SpyreMethodBuilder,
-                spyrecore.spyremethod, 'test',
+                spyrecore.spyremethod, method_name,
                 method_desc)
+
+        method_desc['method'] = 'GET'
+        self.failUnlessRaises(errors.SpyreMethodBuilder,
+                spyrecore.spyremethod, method_name,
+                method_desc)
+
+        method_desc['path'] = '/:username'
+        method = spyrecore.spyremethod(method_name, method_desc)
+        self.assertTrue(method)
+
+    def test_optional_attr(self):
+        method_name = 'test'
+        method_desc = {'method': 'GET', 'path': '/:username',
+                'required_params': ['username']}
+
+        method = spyrecore.spyremethod(method_name, method_desc)
+        self.assertTrue(method)
+
+        self.failUnlessRaises(errors.SpyreMethodCall, method, {})
+
+    def test_callable(self):
+        pass
