82bbb2224c3324cd8d372f529244b7d719f23687
franck cuny <franck.cuny@gmail.com>
2011-10-09 16:34:04 -0700

    build env with base_url, parse url, base_url, etc etc

--- spyre/spyrecore.py
+++ spyre/spyrecore.py
@@ -1,33 +1,52 @@
 import __future__
 import re
+from urlparse import urlparse
 from spyre import errors
+from spyre import request
 
 
 class spyrecore(object):
-    def __init__(self, json):
+    def __init__(self, json, base_url):
         self.json = json
-        self._init_meta(json)
+        self.base_url = None
+
+        self._init_meta(json, base_url)
+
         self._init_methods(json)
 
-    def _init_meta(self, json):
+    def _init_meta(self, json, base_url):
         self.name = json['name']
+
+        if 'base_url' in json:
+            self.base_url = json['base_url']
+        elif base_url is not None:
+            self.base_url = base_url
+        else:
+            raise "NON"
 
     def _init_methods(self, json):
         for method_name, method_desc in json['methods'].iteritems():
             self._attach_method(method_name, method_desc)
 
     def _attach_method(self, method_name, method_desc):
-        setattr(self, method_name, spyremethod(method_name, method_desc))
+        setattr(self, method_name, spyremethod(method_name, method_desc,
+            self.base_url))
 
 
 class spyremethod(object):
-    def __init__(self, name, desc):
+    def __init__(self, name, desc, base_url):
         self.name = name
 
         self.method = None
         self.path = None
+        self.base_url = None
         self.required_params = []
         self.optional_params = []
+
+        if 'base_url' in desc:
+            self.base_url = desc['base_url']
+        else:
+            self.base_url = base_url
 
         self._init_args(desc)
 
@@ -51,17 +70,18 @@ class spyremethod(object):
 
         self._check_required_params(args)
 
-        base_url = self._base_url()
+        base_url = urlparse(self.base_url)
 
         # XXX build auth
         auth = self._build_auth()
         params = self._build_params()
         formats = self._build_formats()
+
         env = {
                 'REQUEST_METHOD': self.method,
-                'SERVER_NAME': base_url,
-                'SERVER_PORT': base_url,
-                'SCRIPT_NAME': base_url,
+                'SERVER_NAME': base_url.hostname,
+                'SERVER_PORT': base_url.port,
+                'SCRIPT_NAME': base_url.path,
                 'PATH_INFO': self.path,
                 'REQUEST_URI': '',
                 'QUERY_STRING': '',
@@ -75,6 +95,17 @@ class spyremethod(object):
                 'spore.userinfo': base_url,
                 'spore.formats': formats,
                 }
+
+        http_response = request.spyrerequest(env)
+        print http_response
+        #my $code = $response->status;
+
+        #my $ok = ($method->has_expected_status)
+            #? $method->find_expected_status( sub { $_ eq $code } )
+            #: $response->is_success; # only 2xx is success
+        #die $response if not $ok;
+
+        #$response;
         return 5
 
     def _build_auth(self):
