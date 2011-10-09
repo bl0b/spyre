fd4560482ed305dbde762b7546e6e24d7879fa4e
franck cuny <franck.cuny@gmail.com>
2011-10-09 15:02:39 -0700

    add new errors

--- spyre/errors.py
+++ spyre/errors.py
@@ -7,3 +7,15 @@ class SpyreMethodBuilder(SpyreError):
         self.expr = expr
         msg = "'%s' is required" % expr
         self.msg = msg
+
+
+class SpyreMethodCall(SpyreError):
+    def __init__(self, expr):
+        self.expr = expr
+        msg = "'%s' is rquired" % expr
+        self.msg = msg
+
+
+class SpyreMethodPayload(SpyreError):
+    def __init__(self):
+        self.msg = "NON"
