9bf5aa967c833afe10b8580dc5db6df3aa2b31a3
franck cuny <franck.cuny@gmail.com>
2011-11-13 14:41:45 -0800

    update tests

--- tests/test_spyre_method.py
+++ tests/test_spyre_method.py
@@ -7,12 +7,12 @@ import os.path
 
 MY_DIR = os.path.dirname(__file__)
 spec_file = MY_DIR + "/specs/api.json"
-base_url = 'http://github.com/api/v2/'
+base_url = 'http://github.com/api/v2'
 f = open(spec_file, 'r')
 spec_str = f.read()
 f.close()
 base = base(spec_str, base_url)
-        
+
 class TestSpyreMethod(TestCase):
 
     def test_required_attr(self):
@@ -28,13 +28,15 @@ class TestSpyreMethod(TestCase):
                 Method, method_name,
                 method_desc, base)
 
-        method_desc['path'] = '/:username'
+        method_desc['path'] = '/user/:username'
+        method_desc['required_params'] = ['username']
         method_desc['expected_status'] = [200, 404]
 
         method = Method(method_name, method_desc, base)
         self.assertTrue(method)
-        resp = method({})
+        resp = method(username='bar')
         self.assertEqual(resp.status, '404')
+	self.assertTrue(False)
 
     def test_optional_attr(self):
         method_name = 'test'
@@ -45,7 +47,7 @@ class TestSpyreMethod(TestCase):
         method = Method(method_name, method_desc, base)
         self.assertTrue(method)
 
-        self.failUnlessRaises(errors.SpyreMethodCall, method, {})
+        self.failUnlessRaises(errors.SpyreMethodCall, method)
 
     def test_callable(self):
         pass
