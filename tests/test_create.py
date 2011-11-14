ae7ae53edd52edb8a1290d44a1d2463de2508428
franck cuny <franck.cuny@gmail.com>
2011-11-13 17:19:15 -0800

    add a test for non-existing spec file

--- tests/test_create.py
+++ tests/test_create.py
@@ -21,17 +21,20 @@ class TestSpyre(TestCase):
 
     def test_new_from_spec(self):
         spore = Spyre().new_from_spec(spec_file, base_url)
+
         self.assertTrue(spore)
+        self.assertEqual(spore.base_url, base_url)
         self.assertEqual(spore.name, "Test API")
+
         resp = spore.get_user_info(username='franckcuny')
         self.assertIsInstance(resp, Response)
-
-        spore = Spyre().new_from_spec(spec_file, base_url)
-        self.assertTrue(spore)
 
     def test_new_from_string(self):
         f = open(spec_file, 'r')
         spec_str = f.read()
         f.close()
-        spore = Spyre().new_from_string(spec_str, base_url)
+        spore = Spyre().new_from_string(spec_str)
         self.assertTrue(spore)
+
+    def test_inexisting_spec(self):
+        self.assertRaises(errors.SpyreObjectBuilder, Spyre.new_from_spec, ('foo'))
