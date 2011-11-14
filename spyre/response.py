0415896607807950cd150be415d34250b5fa98b9
franck cuny <franck.cuny@gmail.com>
2011-11-13 16:55:59 -0800

    add more attributes to response class

--- spyre/response.py
+++ spyre/response.py
@@ -3,5 +3,16 @@ class Response(object):
     def __init__(self, env, status, resp, content):
         self.env = env
         self.status = status
+        self.code = status
         self.resp = resp
         self.content = content
+        self.content_type = resp.get('content-type')
+        self.content_length = resp.get('content-length')
+
+        if status >= 200 and status < 300:
+            self.is_success = True
+        else:
+            self.is_success = False
+
+    def finalize(self):
+        pass
