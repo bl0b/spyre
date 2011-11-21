122e4bc7e872711ae600cd923fe982442a9091af
franck cuny <franck.cuny@gmail.com>
2011-11-20 20:54:08 -0800

    remove initial code for spyre

--- py/spyre/__init__.py
+++
@@ -1,4 +0,0 @@
-from spore import *
-
-# Yay, init !
-__all__ = ['new_from_spec']
--- py/spyre/response.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-
-class Response:
-    def __init__(self, env):
-        self.status = 200
-        self.code = self.status
-        self.env = env
-        
